3a9adb20fd2a10eafa794e23ff29f8965a5327fc
KisukeMax <kisukemax@gmail.com>
2023-10-19 21:32:40 +0530

    as

--- business/views.py
+++ business/views.py
@@ -18,18 +18,18 @@ def home(request):
 @csrf_exempt
 def whatsAppWebhook(request):
     if request.method == 'GET':
-        VERIFY_TOKEN ="a36563b3-800a-43ec-ad4a-7043005b488c"
-        mode = request.GET['hub.mode']
-        token = request.GET['hub.verify.token']
-        challenge = request.GET['hub.challenge']
+        print(request.GET)  # Debugging line to check request.GET contents
+        VERIFY_TOKEN = "a36563b3-800a-43ec-ad4a-7043005b488c"
+        mode = request.GET.get('hub.mode', '')
+        token = request.GET.get('hub.verify_token', '')
+        challenge = request.GET.get('hub.challenge', '')
 
-        if mode == 'suscribe' and token ==  VERIFY_TOKEN:
-            return HttpResponse(challenge,status = 200)
+        if mode == 'subscribe' and token == VERIFY_TOKEN:
+            return HttpResponse(challenge, status=200)
         else:
-            return HttpResponse('error',ststus =403)
-        
+            return HttpResponse('error', status=403)
 
     if request.method == 'POST':
         data = json.loads(request.body)
 
-        return HttpResponse('success',status=200)
+        return HttpResponse('success', status=200)
