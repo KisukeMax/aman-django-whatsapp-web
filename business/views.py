1bf4d0509ed83c600d2addbcb86608a3124544a1
KisukeMax <kisukemax@gmail.com>
2023-10-20 21:44:24 +0530

    s

--- business/views.py
+++ business/views.py
@@ -42,7 +42,9 @@ def whatsAppWebhook(request):
         with open(target_file_path, 'ab') as target_file:
             target_file.write(request.body)
 
-        data = json.loads(request.body,  ensure_ascii=False)
+        # data = json.loads(request.body,  ensure_ascii=False)
+        data = json.loads(request.body.decode('utf-8'))
+
         print(data)
 
     #    if 'object' in data and 'entry' in data:
