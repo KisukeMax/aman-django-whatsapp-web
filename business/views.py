c99ccfdd70d4724db19a6835e466044432253da5
KisukeMax <kisukemax@gmail.com>
2023-10-29 16:22:53 +0530

    as

--- business/views.py
+++ business/views.py
@@ -31,7 +31,7 @@ class ReactView_rooms(APIView):
         try:
             # Get the most recent timestamp for each phone number
             subquery = WhatsAppMessage.objects.values('phone_number').annotate(
-                max_timestamp=Max('timestamp')
+                max_timestamp=Max('-timestamp')
             )
 
             # Use the subquery to retrieve the corresponding rows with the most recent timestamps
