6006b52aca208eb54683ae295114fe9d40c2073e
KisukeMax <kisukemax@gmail.com>
2023-11-09 00:00:29 +0530

    a

--- business/serializer.py
+++ business/serializer.py
@@ -5,4 +5,4 @@ from .models import *
 class ReactSerializer(serializers.ModelSerializer):
     class Meta:
         model = WhatsAppMessage
-        fields = ["phone_id" , "whatsapp_id" , "from_id" , "timestamp" , "profile_name", "phone_number" , "text", "message_text_sent_by", "msg_status_code", "upload_media_path", "fb_media_id"]
+        fields = ["phone_id" , "whatsapp_id" , "from_id" , "timestamp" , "profile_name", "phone_number" , "text", "message_text_sent_by", "msg_status_code", "upload_media_path", "fb_media_id", "msg_seen_by_admin"]
