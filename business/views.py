45cf1485e72dcc9e6a5421a3045c943bd2dc80bd
KisukeMax <kisukemax@gmail.com>
2023-11-15 21:45:25 +0530

    upload video and docs

--- business/views.py
+++ business/views.py
@@ -276,6 +276,39 @@ def upload_document(request):
         return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
+@api_view(['POST'])
+def upload_video(request):
+    document = request.data.get('video')
+    profile_name = request.data.get('profile_name')
+    phone_number = request.data.get('phone_number')
+    # print(document)
+    print(request.data)
+    
+    if not document:
+        return Response({'error': 'No document data received'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    # Define the directory where you want to save the uploaded documents within the static folder
+    upload_dir = os.path.join(settings.STATIC_ROOT, 'business', 'uploads', 'video')
+
+    # Ensure the directory exists
+    print(upload_dir)
+    os.makedirs(upload_dir, exist_ok=True)
+
+    try:
+        doc_path = os.path.join(upload_dir, document.name)
+
+        with open(doc_path, 'wb') as file:
+            file.write(document.read())
+        
+        send_and_upload_video(doc_path,profile_name,phone_number)
+        # You can now process the uploaded image, e.g., save the path to a database or perform other operations
+
+        return Response({'message': 'document uploaded successfully'}, status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+
 
 
 @api_view(['POST'])
--- test.py
+++ test.py
@@ -28,3 +28,20 @@
 #     print('Error uploading image')
 #     print(response.text)
 
+
+
+import requests
+
+# Define the URL of your Django REST API endpoint
+url = 'https://django.lightshoes.in/update_msg_seen'  # Replace with the actual URL of your API
+
+
+data = {"whatsapp_id": "wamid.HBgMOTE5OTU2OTI5MzcyFQIAERgSMDRCQkM0OTM0NzgyNjE1OEZBAA=="}
+
+response = requests.post(url, json=data)
+
+# Check the response from the API
+if response.status_code == 200:
+    print("Message marked as seen by admin successfully.")
+else:
+    print("Error:", response.status_code, response.text)
