54a4edb86e6f06695ae4db34ccb8947cd7eeba0e
KisukeMax <kisukemax@gmail.com>
2024-01-15 23:03:49 +0530

    image

--- business/views.py
+++ business/views.py
@@ -419,6 +419,17 @@ def send_rest_template(request):
                 return Response({'message': 'document uploaded successfully'}, status=status.HTTP_200_OK)
             else:
                 return Response({'error': 'Error uploading media'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            
+        if data.get("template_name") == "business_start_chat_photo":
+            # print(data)
+            media_path = upload_parser_media(request)
+            if media_path:
+                print("uploaded at - ", media_path)
+                send_business_start_chat_photo(media_path, data)
+                # You can use media_path as needed, e.g., pass it to another function or save it to a database
+                return Response({'message': 'document uploaded successfully'}, status=status.HTTP_200_OK)
+            else:
+                return Response({'error': 'Error uploading media'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
     except Exception as e:
