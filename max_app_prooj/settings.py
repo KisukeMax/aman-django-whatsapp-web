e9437aadc388c84beb3beb7c0b8c1c65be8aa2db
KisukeMax <kisukemax@gmail.com>
2024-02-11 15:38:20 +0530

    a

--- max_app_prooj/settings.py
+++ max_app_prooj/settings.py
@@ -135,7 +135,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-AUTHENTICATION_BACKENDS = ['business.backends.CustomBackend']
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
